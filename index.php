33e45f3e40cfcab540b49a7416aee062c2bc0c4c
Lucas <lucas.81.gabriel@gmail.com>
2023-10-11 19:53:08 -0300

    terceiro commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/lib/icons/menu-icon.png">
-    <title>Dashboard - Aplicativos</title>
+    <title>lsbrel - home page</title>
     <!-- Bootstrap 5 -->
     <link rel="stylesheet" href="./resources/bootstrap/bootstrap.min.css" />
     <!-- css de transition-->
