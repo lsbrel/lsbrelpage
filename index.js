25c853dfb95e676e42fa980f396d79abb28344fc
Lucas <lucas.81.gabriel@gmail.com>
2023-10-11 19:01:14 -0300

    priera commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// Pages
+
+
+const routes = [
+    //{ path: '/', redirect: '/pesquisa'},
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes
+})
+const app = Vue.createApp({
+    data() {
+        return {
+            direction: 0,
+            componente: window.location.hash
+        }
+    },
+    components:{
+        'main-menu': mainMenu,
+    },
+    computed: {
+        username() {
+            return this.$route.params.username
+          }
+    },
+    methods:{
+
+    },
+    mounted() {
+        window.addEventListener('hashchange', () => {
+            this.componente = window.location.hash
+        })
+        window.addEventListener('touchstart',(start)=>{
+            this.direction += start.touches[0].clientX
+        })
+        window.addEventListener('touchend', (end)=>{
+            this.direction -= end.changedTouches[0].clientX
+            this.touchMudarRotas()
+            this.direction = 0
+        })
+    },
+    template:`
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 m-0 p-0" style="overflow: clip">
+                <transition mode="out-in" name="slide-fade">
+                    <router-view></router-view>
+                </transition>
+            </div>
+            <div class="gx-0 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                
+            </div>
+        </div>
+    </div>  
+    `
+})
+
+app.use(router)
+app.mount('#app')
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/lib/icons/menu-icon.png">
+    <title>Dashboard - Aplicativos</title>
+    <!-- Bootstrap 5 -->
+    <link rel="stylesheet" href="./resources/bootstrap/bootstrap.min.css" />
+    <!-- css de transition-->
+    <link rel="stylesheet" href="./lib/css/utilites.css" />
+    <link rel="stylesheet" href="./lib/css/main.css" />
+    <!-- Axios e SweetAlert -->
+    <script src="./resources/axios/axios.min.js"></script>
+    <!-- Google icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+<body>
+    <div id="app"></div>
+</body>
+<!-- Arquivo principal do vuejs -->
+<script type="module" src="./index.js"></script>
+<!-- VUE e VUETIFY -->
+<script src="./resources/vue/vue.global.prod.js"></script>
+<script src="./resources/vue/vue-router.js"></script>
+</html>
